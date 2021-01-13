cbf8190fc4549134dac2e8c9418b7fde08ebbbb3
Michael <michael@pop-os.localdomain>
2021-01-13 15:57:02 -0500

    Shell prompt made into function

--- shell.c
+++ shell.c
@@ -4,13 +4,27 @@
 #include <malloc.h>
 #include <unistd.h>
 
+void PrintPrompt();
+
 int main(int argc, char const *argv[])
 {
 	size_t size = 10;
 	char * buffer;
 	do
 	{
-		int host;
+		PrintPrompt();
+
+		buffer = (char *) malloc (size);
+
+		getline (&buffer, &size, stdin);
+	}
+	while (strcmp(buffer, "exit") != 0);
+	return 0;
+}
+
+void PrintPrompt()
+{
+	int host;
 		char hostname[256];
 		char * user = getenv("USER");
 		host = gethostname(hostname, sizeof(hostname));
@@ -19,11 +33,4 @@ int main(int argc, char const *argv[])
 			printf("error getting hostname\n");
 
 		printf("%s@[%s] > ",user,hostname);
-
-		buffer = (char *) malloc (size);
-
-		getline (&buffer, &size, stdin);
-	}
-	while (strcmp(buffer, "exit") != 0);
-	return 0;
 }
