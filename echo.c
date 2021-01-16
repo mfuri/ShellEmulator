af76870b2b02be9f962154356c098dc2509e7c5c
sjm49 <48703138+sjm49@users.noreply.github.com>
2021-01-16 15:05:37 -0500

    Update echo.c

--- echo.c
+++ echo.c
@@ -2,22 +2,23 @@ void echo(tokenlist *tokens){
 
     for (int i=1;i<tokens->size;i++){ //iterate through argument tokens
         
-        int chk; 
         char q;
         q=tokens->items[i][0];
         char r;
         r='$';
-        if (q==r){
-  
+        if (q==r){.    //check for env var
+        //remove $ char from token and get corresponding env var
             char * arg= (char*) malloc(sizeof(tokens->items[i])-1);
-            arg=&tokens->items[i][1];
+            arg=&tokens->items[i][1]; 
            char * env=getenv(arg);
            tokens->items[i]=env;   
+         //update token list with expanded token
+     
            }      
 
         }
     
-    for (int i=0;i<tokens->size;i++){
+    for (int i=0;i<tokens->size;i++){ 
         printf("%s ",tokens->items[i]);
     }
   }
