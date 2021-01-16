6cf973d5572b187f899597a630dc64babddab430
sjm49 <48703138+sjm49@users.noreply.github.com>
2021-01-16 17:19:39 -0500

    read first token to designate proper command
    get_command takes tokenlist and returns pointer to proper command function. works for echo and (preliminary) exit and error handling for incorrect commands or those not yet implemented. will update function to loop through list of possible commands rather than hard coding a few examples. Once path search is implemented, rather than just printing error if command not found, try searching for path, if thats unsuccessful -> error.

---
+++ get_command.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+
+typedef void (*fnptr)();
+fnptr get_command(tokenlist *tokens);
+
+void commanderr();
+void ex();
+
+fnptr get_command(tokenlist *tokens){
+    char * command=tokens->items[0];
+    if (strcmp(command,"echo")==0)
+        return echo;
+    if (strcmp(command, "exit")==0)
+        return ex;
+
+    else
+        return commanderr;
+}
+      
+  
+void commanderr(){
+    printf("\n%s\n", "Error: command not found");
+}
+
+void ex(){
+    //check for background process
+    //implement timer stuff
+    printf("\n%s\n", "Shell ran for ...");
+}
