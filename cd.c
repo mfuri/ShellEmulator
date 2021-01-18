0e315b703716086252399dcc03f14e743f8f7539
sjm49 <48703138+sjm49@users.noreply.github.com>
2021-01-18 18:29:28 -0500

    added correct format/path expansion if arg is a subdir

--- cd.c
+++ cd.c
@@ -1,28 +1,37 @@
 //Does not currently account for ".." 
-//if changing to a subdirectory of pwd, new prompt pwd shows just subdirectory rather than whole path 
+//updated to show full path after change and handle different input formats
+
     
-void cd(tokenlist *tokens){  
+void cd(tokenlist *tokens){
     char * path;
     char * pwd;
+    char * home;
+ 
     pwd=getenv("PWD");
-
-    if (tokens->size==1){
-        path=getenv("HOME");
-        
-             
+    home=getenv("HOME");
+    
+    if (tokens->size==1){                               //If no argument, cd to home
+        path= (char*)malloc(strlen(home)+1);
+        strcpy(path,home);            
     }
-    else {
+    else {                                              //otherwise get user input
         path= (char *) malloc(strlen(tokens->items[1])+1);
         strcpy(path, tokens->items[1]);
+              
+        if (strstr(path,home)==NULL){                   //if not a complete path, add home to start of path
+            path = (char*) realloc(path, sizeof(path)+strlen(home)+1);
+            strcpy(path,home);
+            strcat(path,"/");
+            strcat(path,tokens->items[1]);
+                 
+        }
         if (access(path,X_OK)!=0){
             printf("\n%s\n", "Directory not found");
+             printf("\n%s\n", path);
             return;
         }
-
     }
-    strcpy(pwd,path);
-   
-
+    strcpy(pwd,path);           //change cwd
+    free(path);  
     return;
-   
-    }
+}
