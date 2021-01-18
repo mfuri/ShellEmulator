8e016678005c4b7765630229213ee9f0613d0a8d
sjm49 <48703138+sjm49@users.noreply.github.com>
2021-01-17 20:26:58 -0500

    split into separate funcs

--- env_expand+echo.c
+++ env_expand+echo.c
@@ -1,30 +1,32 @@
-void echo(tokenlist *tokens){
 
+void env_expand(tokenlist *tokens){
     for (int i=1;i<tokens->size;i++){ //iterate through argument tokens
         
         char q;
         q=tokens->items[i][0];
         char r;
         r='$';
-        if (q==r){.    //check for env var
+        if (q==r){    //check for env var
         
             //remove $ char from token and get corresponding env var
-
             char * arg= (char*) malloc(sizeof(tokens->items[i])-1);
             strcpy(arg,&tokens->items[i][1]);
-         
+            
+
             char * env=getenv(arg);
-            tokens->items[i]=realloc(tokens->items[i],sizeof(env)+1);
+            tokens->items[i]=realloc(tokens->items[i],strlen(env)+1);
+
             strcpy(tokens->items[i],env);   
-
-            free(arg); 
-         //update token list with expanded token
      
-           }      
-
+            free(arg); 
         }
+    }    
+}   
+//echo fn
+void echo(tokenlist *tokens){
     
-    for (int i=1;i<tokens->size;i++){ 
+    for (int i=1;i<tokens->size;i++){
         printf("%s ",tokens->items[i]);
     }
+    printf("\n");
   }
