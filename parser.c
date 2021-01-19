8ad95c665d5db292d9db46d13f97b57bc7369d65
sjm49 <48703138+sjm49@users.noreply.github.com>
2021-01-19 18:22:04 -0500

    added cd

--- parser.c
+++ parser.c
@@ -29,6 +29,10 @@ void PrintPrompt();
 char * EnvExpand(char * input);
 bool is_Path(char *);
 char * TildeExpand(char * input);
+void cd(tokenlist *tokens)
+
+
+
 
 
 int main()
@@ -193,4 +197,43 @@ char * TildeExpand(char * input)
     strcpy(output, p.we_wordv[0]);
     wordfree(&p);
     return output;
+}
+
+
+void cd(tokenlist *tokens){.    //Does not currently account for ".." 
+				//updated to show full path after change and handle different input formats
+    char * path;
+    char * pwd;
+    char * home;
+ 
+    tokens->items[1]=TildeExpand(tokens->items[1]);  //this line added to expand path within fn using TildeExpand on single token 
+			   				//alternatively - expand in main with tilda_expand on all tokens (i can't spell tilde apparently lol)
+			  
+    pwd=getenv("PWD");
+    home=getenv("HOME");
+    			   
+    if (tokens->size==1){                               //If no argument, cd to home
+        path= (char*)malloc(strlen(home)+1);
+        strcpy(path,home);            
+    }
+    else {                                              //otherwise get arg
+        path= (char *) malloc(strlen(tokens->items[1])+1);
+        strcpy(path, tokens->items[1]);
+              
+        if (strstr(path,home)==NULL){                   //if not a complete path, add home to start of path
+            path = (char*) realloc(path, sizeof(path)+strlen(home)+1);
+            strcpy(path,home);
+            strcat(path,"/");
+            strcat(path,tokens->items[1]);
+                 
+        }
+        if (access(path,X_OK)!=0){
+            printf("\n%s\n", "Directory not found");
+             printf("\n%s\n", path);
+            return;                                 //print error and exit if path not an accesible directory
+        }
+    }
+    strcpy(pwd,path);           //change cwd
+    free(path);  
+    return;
 }
