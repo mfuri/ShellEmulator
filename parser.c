08821e19d86753669907f8c05e0f790c028e9496
Michael T <tofasttafuri@gmail.com>
2021-02-07 16:49:12 -0500

    removed path from parser.c

--- parser.c
+++ parser.c
@@ -118,48 +118,6 @@ void time_command(time_t START,time_t STOP) //checks current run time and update
 
 }
 
-bool is_Path(tokenlist * tokens)
-{
-    
-    char * input=tokens->items[0];
-    char * path = getenv("PATH");
-
-    char * pathcopy = (char *)malloc(strlen(path)+1);
-    strcpy(pathcopy,path);
-
-    char * token = strtok(pathcopy,":");
-
-    char * fpath= (char *) malloc(strlen(token)+strlen(input)+2);
-
-    while (token != NULL)
-    {   
-        fpath=(char*) realloc(fpath,strlen(token)+strlen(input)+2);
-        strcpy(fpath,token);
-        strcat(fpath,"/");
-        strcat(fpath,input);
-        //printf("loop");
-
-        if( access(fpath, X_OK)!=0){
-            //printf( " %s\n", token ); //printing each token
-            token = strtok(NULL, ":");
-            
-            continue;
-        }
-
-        tokens->items[0] = (char*) realloc(tokens->items[0], strlen(fpath)+1);
-        strcpy(tokens->items[0], fpath);
-
-        free(fpath);
-        free(pathcopy);
-        return true;
-
-    }
-
-    
-    free(fpath);
-    free(pathcopy);
-    return false;
-}
 
 void external_cmd(tokenlist * tokens,bool bg, bool io){ 
 
