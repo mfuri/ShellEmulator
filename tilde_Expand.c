631937db41eb919df60b5907876d255025238659
Michael T <tofasttafuri@gmail.com>
2021-02-08 18:39:17 -0500

    Update tilde_Expand.c

--- tilde_Expand.c
+++ tilde_Expand.c
@@ -8,16 +8,28 @@ void tilde_Expand(tokenlist *tokens)
 		//Check to see if first character is ~
 		//Expands using wordexp
 		if (tokens->items[i][0] == '~')
-		{
-			wordexp_t p;
-			int size;
-			wordexp(tokens->items[i], &p, 0);
-			size = strlen(p.we_wordv[0]);
-			
-			tokens->items[i] = (char *) realloc(tokens->items[i], size);
-			
-			strcpy(tokens->items[i], p.we_wordv[0]);
-			wordfree(&p);
-		}
+			{
+				tokens->items[i]++;
+				char * home = getenv("HOME");
+				char * buf = (char *) malloc(strlen(tokens->items[i])+strlen(home));
+				
+				strcpy(buf, home);
+				strcat(buf, tokens->items[i]);
+				
+				tokens->items[i] = (char *) realloc(tokens->items[i], strlen(tokens->items[i])+strlen(home));
+				
+				strcpy(tokens->items[i], buf);
+				
+				free(buf);
+				
+				/*wordexp_t p;
+				int size;
+				wordexp(tokens->items[i], &p, 0);
+				size = strlen(p.we_wordv[0]);
+				
+				
+				strcpy(tokens->items[i], p.we_wordv[0]);
+				wordfree(&p);*/
+			}
 	}
 }
